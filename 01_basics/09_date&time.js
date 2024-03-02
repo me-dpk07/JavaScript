b81bd5947f58a8e4683cf4c2c66bc64617b02947
me-dpk07 <159159359+me-dpk07@users.noreply.github.com>
2024-03-02 13:16:45 +0000

    changes

--- 01_basics/09_date&time.js
+++ 01_basics/09_date&time.js
@@ -29,6 +29,5 @@ newDate.toLocaleString('default', {    // adding new methods to localestring & w
     weekday: "long",
     
 })
-
-
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+
