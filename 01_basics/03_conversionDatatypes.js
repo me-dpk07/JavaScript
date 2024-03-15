f76290ebefdf2641e764be68878e33b0681debd0
me-dpk07 <159159359+me-dpk07@users.noreply.github.com>
2024-03-15 10:06:36 +0000

    conversion

--- 01_basics/03_conversionDatatypes.js
+++ 01_basics/03_conversionDatatypes.js
@@ -51,7 +51,7 @@ console.log(negValue);
 // console.log(2+2);
 // console.log(2-2);
 // console.log(2*2);
-// console.log(2**3);
+// console.log(2**3);~
 // console.log(2/3);
 // console.log(2%3);
 
